1b341f334209112f6d684311422b0b365d7791d0
Ramon Rodrigues <cerberus.ramon@gmail.com>
2016-04-29 13:12:40 +0100

    Added linter dependency installer

---
+++ sublime/linter.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#!/home/ramon/.rbenv/shims/ruby
+
+# This will install SublimeLinter Dependencies
+
+npm install -g coffeelint jshint sass-lint;
+/home/ramon/.rbenv/shims/gem install slim_lint
